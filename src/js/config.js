4be3abfdc97c4bdf08727302b5c8a49e436ab761
daviles82 <avileshouston@live.com>
2024-08-31 09:23:21 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c367e8d4-0d66-481a-8d63-5637c8d97750';
+export const MODAL_CLOSE_SEC = 2.5;
